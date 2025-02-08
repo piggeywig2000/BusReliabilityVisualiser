aa78b637afec2680f78f8ee1d9757ba7b29152f4
Theo <32236823+piggeywig2000@users.noreply.github.com>
2025-02-08 13:49:39 +0000

    Implemented distance-based interpolation for order resolution

--- BusReliabilityScraper/Map/Route.cs
+++ BusReliabilityScraper/Map/Route.cs
@@ -79,18 +79,34 @@ namespace BusReliabilityScraper.Map
                     .Zip(throwAfterIndexes.Skip(1), (iFrom, iTo) => (iFrom, iTo))
                     .Sum(p => Math.Abs(points[p.iTo].RouteDistance - points[p.iFrom].RouteDistance));
 
-                // Throw away the points with the lowest cost, and re-distribute them evenly along the section
-                // TODO: Consider re-distributing based on how far each point travelled from the last
+                // Throw away the points with the lowest cost, and re-distribute them along the section based on distance
                 int throwRangeStart = (throwBeforeCost < throwAfterCost) ? throwBeforeIndexes[0] : throwAfterIndexes[0];
                 int throwRangeEnd = (throwBeforeCost < throwAfterCost) ? throwBeforeIndexes[^1] + 1 : throwAfterIndexes[^1] + 1; // exclusive
                 Console.WriteLine($"Throwing away {throwRangeEnd - throwRangeStart} points, from {throwRangeStart} to {throwRangeEnd}");
 
+                // Set up values used for distance-based interpolation, if we're interpolating
+                double totalThrowDistance = 0;
+                double cumulativeThrowDistance = 0;
+                if (throwRangeStart > 0 && throwRangeEnd < points.Count)
+                {
+                    totalThrowDistance = Enumerable.Range(throwRangeStart, throwRangeEnd - throwRangeStart + 1)
+                        .Sum(i => points[i - 1].Point.DistanceTo(points[i].Point));
+                }
+                if (totalThrowDistance == 0)
+                    totalThrowDistance = 1; // Prevent dividing by 0 if all points at same location for whatever reason (hopefully can't happen)
+
+                // Re-distribute points and update position
                 for (int i = throwRangeStart; i < throwRangeEnd; i++)
                 {
-                    points[i].RouteDistance =
-                        throwRangeStart == 0 ? points[throwRangeStart].RouteDistance : (
-                        throwRangeEnd == points.Count ? points[throwRangeEnd - 1].RouteDistance :
-                        points[throwRangeStart - 1].RouteDistance.LerpToUnclamped(points[throwRangeEnd].RouteDistance, (i - throwRangeStart + 1.0) / (throwRangeEnd - throwRangeStart + 1.0)));
+                    if (throwRangeStart == 0)
+                        points[i].RouteDistance = points[throwRangeStart].RouteDistance;
+                    else if (throwRangeEnd == points.Count)
+                        points[i].RouteDistance = points[throwRangeEnd - 1].RouteDistance;
+                    else
+                    {
+                        cumulativeThrowDistance += points[i - 1].Point.DistanceTo(points[i].Point);
+                        points[i].RouteDistance = points[throwRangeStart - 1].RouteDistance.LerpTo(points[throwRangeEnd].RouteDistance, cumulativeThrowDistance / totalThrowDistance);
+                    }
 
                     points[i].MovePositionFromRouteDistance(plannedRoute);
                 }
