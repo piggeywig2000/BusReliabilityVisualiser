3a849b359c51bfea3f40c0ed2792e8e839f05c63
Theo <32236823+piggeywig2000@users.noreply.github.com>
2025-02-27 16:34:15 +0000

    Improved BodsClient async. Made it use cancellation tokens and DisposeAsync

--- BodsDotNet/BodsClient.cs
+++ BodsDotNet/BodsClient.cs
@@ -26,29 +26,29 @@ namespace BodsDotNet
 
         public async Task<Schemas.Timetable.Timetable> GetTimetableById(int id, CancellationToken cancellationToken = default)
         {
-            using HttpResponseMessage httpResponse = await httpClient.GetAsync($"https://data.bus-data.dft.gov.uk/api/v1/dataset/{id}?api_key={apiKey}");
-            using Stream stream = await httpResponse.Content.ReadAsStreamAsync();
+            using HttpResponseMessage httpResponse = await httpClient.GetAsync($"https://data.bus-data.dft.gov.uk/api/v1/dataset/{id}?api_key={apiKey}", cancellationToken);
+            await using Stream stream = await httpResponse.Content.ReadAsStreamAsync(cancellationToken);
 
             if (!httpResponse.IsSuccessStatusCode)
             {
-                Schemas.Timetable.ErrorResponse? response = await JsonSerializer.DeserializeAsync<Schemas.Timetable.ErrorResponse>(stream, jsonSerializerOptions);
+                Schemas.Timetable.ErrorResponse? response = await JsonSerializer.DeserializeAsync<Schemas.Timetable.ErrorResponse>(stream, jsonSerializerOptions, cancellationToken);
                 throw new BodsRequestException(response?.Detail ?? "An error occurred, but there was no description of what it was.", httpResponse.StatusCode);
             }
             else
             {
-                Schemas.Timetable.Timetable? response = await JsonSerializer.DeserializeAsync<Schemas.Timetable.Timetable>(stream, jsonSerializerOptions);
+                Schemas.Timetable.Timetable? response = await JsonSerializer.DeserializeAsync<Schemas.Timetable.Timetable>(stream, jsonSerializerOptions, cancellationToken);
                 return response ?? throw new BodsRequestException("Server returned success, but failed to parse timetable data.", httpResponse.StatusCode);
             }
         }
 
         public async Task DownloadTransXChangeFromUrl(string url, string downloadPath, bool overwrite = true, CancellationToken cancellationToken = default)
         {
-            using HttpResponseMessage httpResponse = await httpClient.GetAsync(url);
+            using HttpResponseMessage httpResponse = await httpClient.GetAsync(url, cancellationToken);
             httpResponse.EnsureSuccessStatusCode();
-            using Stream contentStream = await httpResponse.Content.ReadAsStreamAsync();
+            await using Stream contentStream = await httpResponse.Content.ReadAsStreamAsync(cancellationToken);
 
             using ZipArchive zipArchive = new(contentStream, ZipArchiveMode.Read);
-            zipArchive.ExtractToDirectory(downloadPath, overwrite);
+            await Task.Run(() => zipArchive.ExtractToDirectory(downloadPath, overwrite), cancellationToken); // No async API for this yet, this is a bodge
         }
 
         public async Task<IReadOnlyCollection<Schemas.TransXChange.TransXChange>> GetTransXChangeFromUrl(string url, CancellationToken cancellationToken = default)
@@ -56,19 +56,19 @@ namespace BodsDotNet
             using HttpResponseMessage httpResponse = await httpClient.GetAsync(url, cancellationToken);
             httpResponse.EnsureSuccessStatusCode();
 
-            using Stream contentStream = await httpResponse.Content.ReadAsStreamAsync();
+            await using Stream contentStream = await httpResponse.Content.ReadAsStreamAsync(cancellationToken);
             return await GetTransXChangeFromZipStream(contentStream, cancellationToken);
         }
 
         public async Task<IReadOnlyCollection<Schemas.TransXChange.TransXChange>> GetTransXChangeFromZipFile(string filePath, CancellationToken cancellationToken = default)
         {
-            using FileStream fs = new(filePath, FileMode.Open, FileAccess.Read);
+            await using FileStream fs = new(filePath, FileMode.Open, FileAccess.Read);
             return await GetTransXChangeFromZipStream(fs, cancellationToken);
         }
 
         public async Task<Schemas.TransXChange.TransXChange> GetTransXChangeFromXmlFile(string filePath, CancellationToken cancellationToken = default)
         {
-            using FileStream fs = new(filePath, FileMode.Open, FileAccess.Read);
+            await using FileStream fs = new(filePath, FileMode.Open, FileAccess.Read);
             return await GetTransXChangeFromXmlStream(fs, cancellationToken)
                 ?? throw new XmlException($"Failed to parse the TransXChange file at {Path.GetFileName(filePath)}");
         }
@@ -80,7 +80,7 @@ namespace BodsDotNet
 
             foreach (ZipArchiveEntry entry in zipArchive.Entries.OrderBy(e => e.FullName))
             {
-                using Stream entryStream = entry.Open();
+                await using Stream entryStream = entry.Open();
                 Schemas.TransXChange.TransXChange txc = await GetTransXChangeFromXmlStream(entryStream, cancellationToken)
                     ?? throw new XmlException($"Failed to parse the TransXChange file at {entry.Name}");
                 txcs.Add(txc);
@@ -91,7 +91,7 @@ namespace BodsDotNet
 
         private async Task<Schemas.TransXChange.TransXChange?> GetTransXChangeFromXmlStream(Stream stream, CancellationToken cancellationToken)
         {
-            using MemoryStream memoryStream = new();
+            await using MemoryStream memoryStream = new();
             using StreamReader entryReader = new(memoryStream, System.Text.Encoding.UTF8, true);
             await stream.CopyToAsync(memoryStream, cancellationToken);
             memoryStream.Seek(0, SeekOrigin.Begin);
@@ -103,8 +103,8 @@ namespace BodsDotNet
             using HttpResponseMessage httpResponse = await httpClient.GetAsync($"https://data.bus-data.dft.gov.uk/api/v1/datafeed?operatorRef={string.Join(',', operatorNocs)}&lineRef={line}&api_key={apiKey}", cancellationToken);
             httpResponse.EnsureSuccessStatusCode();
 
-            Stream contentStream = await httpResponse.Content.ReadAsStreamAsync(cancellationToken);
-            using MemoryStream memoryStream = new();
+            await using Stream contentStream = await httpResponse.Content.ReadAsStreamAsync(cancellationToken);
+            await using MemoryStream memoryStream = new();
             using StreamReader entryReader = new(memoryStream, System.Text.Encoding.UTF8, true);
             await contentStream.CopyToAsync(memoryStream, cancellationToken);
             memoryStream.Seek(0, SeekOrigin.Begin);
