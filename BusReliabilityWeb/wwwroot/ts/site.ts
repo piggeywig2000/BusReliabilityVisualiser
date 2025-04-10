e48df231109dfa53b1fa1f9ebca505422bd34a33
Theo <32236823+piggeywig2000@users.noreply.github.com>
2025-04-10 13:27:06 +0100

    Added help buttons

---
+++ BusReliabilityWeb/wwwroot/ts/site.ts
@@ -0,0 +1,9 @@
+﻿for (const helpButton of document.getElementsByClassName("button-help") as HTMLCollectionOf<HTMLButtonElement>) {
+    helpButton.addEventListener("click", () => {
+        const contentsToInsert = helpButton.querySelector("template") as HTMLTemplateElement;
+        const clonedContent = contentsToInsert.content.cloneNode(true);
+        const popupContent = document.getElementById("popup-content") as HTMLDivElement;
+        popupContent.replaceChildren(...clonedContent.childNodes);
+        document.getElementById("popup-background")?.style.removeProperty("display");
+    });
+}
